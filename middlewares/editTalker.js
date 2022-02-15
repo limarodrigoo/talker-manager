41c8e7db993e95a1d4b4821850226d409e307637
Rodrigo Lima <limarodrigoo@outlook.com>
2022-02-15 17:47:25 -0300

    :bento: add function to edit talker

---
+++ middlewares/editTalker.js
@@ -0,0 +1,20 @@
+const fs = require('fs').promise;
+
+const editTalker = async (req, res) => {
+  const { id } = req.params;
+  const { name, age, talk } = req.body;
+  
+  const data = await fs.readFile('./talker.json');
+  const talkers = JSON.parse(data);
+
+  const newTalkers = talkers.filter((t) => t.id !== Number(id));
+
+  const newTalker = { id: Number(id), name, age, talk };
+  newTalkers.push(newTalker);
+
+  await fs.writeFile('./talker.json', JSON.stringify(newTalkers));
+
+  return res.status(200).json(newTalker);
+};
+
+module.exports = editTalker;
