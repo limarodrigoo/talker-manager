000add9c956dfd8dc1cafde2457c543f19c1a0cd
Rodrigo Lima <limarodrigoo@outlook.com>
2022-02-15 17:47:54 -0300

    :bento: add method put to talker/id

--- controller/talkerController.js
+++ controller/talkerController.js
@@ -7,6 +7,7 @@ const {
   rateAuth, 
   talkObjAuth, 
   createTalker } = require('../middlewares/creatreTalkerAuth');
+const editTalker = require('../middlewares/editTalker');
 const getAllTalkers = require('../middlewares/getAllTalkers');
 const getTalkerById = require('../middlewares/getTalkerById');
 
@@ -15,5 +16,6 @@ const router = express.Router();
 router.get('/', getAllTalkers);
 router.get('/:id', getTalkerById);
 router.post('/', tokenAuth, nameAuth, ageAuth, talkObjAuth, watchedAtAuth, rateAuth, createTalker);
+router.put('/:id', tokenAuth, nameAuth, ageAuth, talkObjAuth, watchedAtAuth, rateAuth, editTalker);
 
 module.exports = router;
