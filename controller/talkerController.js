3ca1ef8de453d728eef0ada1b20eb63b01fcc943
Rodrigo Lima <limarodrigoo@outlook.com>
2022-02-15 23:36:21 -0300

    :bento: add method delete to talker route

--- controller/talkerController.js
+++ controller/talkerController.js
@@ -7,6 +7,7 @@ const {
   rateAuth, 
   talkObjAuth, 
   createTalker } = require('../middlewares/creatreTalkerAuth');
+const deleteTalker = require('../middlewares/deleteTalker');
 const editTalker = require('../middlewares/editTalker');
 const getAllTalkers = require('../middlewares/getAllTalkers');
 const getTalkerById = require('../middlewares/getTalkerById');
@@ -17,5 +18,6 @@ router.get('/', getAllTalkers);
 router.get('/:id', getTalkerById);
 router.post('/', tokenAuth, nameAuth, ageAuth, talkObjAuth, watchedAtAuth, rateAuth, createTalker);
 router.put('/:id', tokenAuth, nameAuth, ageAuth, talkObjAuth, watchedAtAuth, rateAuth, editTalker);
+router.delete('/:id', tokenAuth, deleteTalker);
 
 module.exports = router;
